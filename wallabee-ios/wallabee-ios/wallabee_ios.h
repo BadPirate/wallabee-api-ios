d8bc8bbe8e2d50929779303668e02591c956e639
Kevin Lohman <badpirate@gmail.com>
2012-04-21 09:38:17 -0700

    Initial Project Structure
    Static Library, xCode 4.3.2, ARC

---
+++ wallabee-ios/wallabee-ios/wallabee_ios.h
@@ -0,0 +1,13 @@
+//
+//  wallabee_ios.h
+//  wallabee-ios
+//
+//  Created by Kevin Lohman on 4/21/12.
+//  Copyright (c) 2012 Good. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface wallabee_ios : NSObject
+
+@end
---
+++ wallabee-ios/wallabee-ios/wallabee_ios.m
@@ -0,0 +1,13 @@
+//
+//  wallabee_ios.m
+//  wallabee-ios
+//
+//  Created by Kevin Lohman on 4/21/12.
+//  Copyright (c) 2012 Good. All rights reserved.
+//
+
+#import "wallabee_ios.h"
+
+@implementation wallabee_ios
+
+@end
